445836b523317377213b17bb11b352a748dd2f78
Brittanie Barrett <britt.barrett302@gmail.com>
2020-06-19 13:55:12 -0600

    work on functions and schema and seed cdata

--- schema.sql
+++ schema.sql
@@ -3,29 +3,30 @@ CREATE database employee_tracker;
 
 USE employee_tracker;
 
-CREATE TABLE employee (
-  id INT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  manager_id INT,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE employee_department (
-  id INT NOT NULL,
+  id INT NOT NULL auto_increment,
   dept_name VARCHAR(30),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE employee_role (
-  id INT NOT NULL,
+  id INT NOT NULL auto_increment,
   title VARCHAR(30),
   salary DEC(10,4),
   employee_department_id INT,
+  constraint fk_employee_department foreign key (employee_department_id) references employee_department (id),
   PRIMARY KEY (id)
 );
 
-SELECT * FROM employee;
-SELECT * FROM employee_department;
-SELECT * FROM employee_role;
+CREATE TABLE employee (
+  id INT NOT NULL auto_increment,
+  full_name VARCHAR(30) NOT NULL,
+  nick_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  constraint fk_employee_role foreign key (role_id) references employee_role (id),
+  manager_id INT,
+  constraint fk_employee foreign key (manager_id) references employee (id),
+  PRIMARY KEY (id)
+);
+
+
--- tracker.js
+++ tracker.js
@@ -5,13 +5,15 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "gopackers12!",
+  password: "",
   database: "employee_tracker",
 });
 
 connection.connect(function (err) {
   if (err) throw err;
-  runTracker();
+  console.log("connected as id " + connection.threadId + "\n");
+
+  //runTracker();
 });
 
 function runTracker() {
@@ -21,30 +23,29 @@ function runTracker() {
       type: "rawlist",
       message: "What would you like to do?",
       choices: [
-        "View all Employees",
+        "View all Employees by Nickname",
+        "View all Employees by Full Name",
         "View all Employees by Department",
-        "View all Employees by Manager",
         "Add Employee",
         "Remove Employee",
         "Update Employee",
         "Update Employee Role",
-        "Update Employee Manager",
         "View all Roles",
         "Add Role",
       ],
     })
     .then(function (answer) {
       switch (answer.action) {
-        case "View all Employees":
-          viewAllEmployees();
+        case "View all Employees by Nickname":
+          viewAllNickname();
+          break;
+
+        case "View all Employees by Full Name":
+          viewAllFullName();
           break;
 
         case "View all Employees by Department":
           viewEmpDept();
-          break;
-
-        case "View all Employees by Manager":
-          viewEmpMngr();
           break;
 
         case "Add Employee":
@@ -78,4 +79,155 @@ function runTracker() {
     });
 }
 
-function viewAllEmployees() {}
+function viewAllNickname() {
+  console.log("Retrieving people that have a fun name...\n");
+  connection.query("SELECT nick_name FROM employee", function (err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+}
+
+function viewAllFullName() {
+  console.log("Retrieving our professional names...\n");
+  connection.query("SELECT full_name FROM employee", function (err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+}
+
+function viewEmpDept() {
+  console.log("Retrieving our people based on department...\n");
+  connection.query("SELECT dept_name FROM employee_department", function (
+    err,
+    res
+  ) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+}
+
+function addEmployee() {
+  console.log("Adding a member to our team...\n");
+  var query = connection.query(
+    "INSERT INTO employee SET ?",
+    {
+      full_name: "",
+      nick_name: "",
+      role_id: "",
+    },
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " employee added!\n");
+      // Call updateemployee AFTER the INSERT completes
+      updateEmployee();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function removeEmployee() {
+  console.log("Trimming the fat...\n");
+  connection.query(
+    "DELETE FROM employee WHERE ?",
+    {
+      full_name: "",
+      nick_name: "",
+      role_id: "",
+      manager_id: "",
+    },
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " employee deleted!\n");
+      // Call viewallfullname AFTER the DELETE completes
+      viewAllFullName();
+    }
+  );
+}
+
+function updateEmployee() {
+  console.log("Updating the employee you selected...\n");
+  var query = connection.query(
+    "UPDATE employee SET ? WHERE ?",
+    [
+      {
+        full_name: "",
+      },
+      {
+        nick_name: "",
+      },
+    ],
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " employee updated!\n");
+
+      //   DO WE HAVE TO DO THIS?
+      //   // Call deleteProduct AFTER the UPDATE completes
+      //   deleteProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function updateEmpRole() {
+  console.log("Updating employee role...\n");
+  var query = connection.query(
+    "UPDATE employee_role SET ? WHERE ?",
+    [
+      {
+        title: "",
+      },
+      {
+        salary: "",
+      },
+    ],
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " employee_role updated!\n");
+      //   // Call deleteProduct AFTER the UPDATE completes
+      //   deleteProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function viewAllRoles() {
+  connection.query("SELECT title FROM employee_role", function (err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+}
+
+function addRole() {
+  console.log("Adding new role to the list...\n");
+  var query = connection.query(
+    "INSERT INTO employee_role SET ?",
+    {
+      title: "",
+      salary: "",
+      employee_dept_id: "50",
+    },
+    function (err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " product inserted!\n");
+      // Call updateemployeerole AFTER the INSERT completes
+      updateEmpRole();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
